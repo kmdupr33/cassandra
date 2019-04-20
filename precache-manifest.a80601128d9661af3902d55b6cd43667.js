2aadc22b20db1a2b770de12e1e1f4b83f4249aeb
Matt Dupree <kmdupr33@gmail.com>
2019-04-20 17:35:52 -0400

    Updates

---
+++ precache-manifest.a80601128d9661af3902d55b6cd43667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52eeb7bf286fccffeda0",
+    "url": "/cassandra/static/js/runtime~main.128eaa26.js"
+  },
+  {
+    "revision": "a4aa715e52043c3bae7b",
+    "url": "/cassandra/static/js/main.f408d437.chunk.js"
+  },
+  {
+    "revision": "4222d7e36d150b845f4b",
+    "url": "/cassandra/static/js/2.231f7d6c.chunk.js"
+  },
+  {
+    "revision": "a4aa715e52043c3bae7b",
+    "url": "/cassandra/static/css/main.c3f496a9.chunk.css"
+  },
+  {
+    "revision": "0801cc77dc87cbb30f3ba7128c1f9736",
+    "url": "/cassandra/index.html"
+  }
+];
